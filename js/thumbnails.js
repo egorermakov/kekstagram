e1a138a559b0ab04911806726f3a40af01e9cb95
Egor Ermakov <im.egorermakov@gmail.com>
2023-03-29 18:47:06 +0300

    Update thumbnails.js

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -5,14 +5,22 @@ const pictureTemplate = document.querySelector('#picture').content;
 const picture = pictureTemplate.querySelector('.picture');
 const photos = generatePhotos();
 
+
+
 const addThumbnails = () => {
+
+  const listFragment = document.createDocumentFragment();
+
   photos.forEach((photo) => {
     const thumbnail = picture.cloneNode(true);
     thumbnail.querySelector('.picture__img').src = photo.url;
     thumbnail.querySelector('.picture__likes').textContent = photo.likes;
     thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
-    list.appendChild(thumbnail);
+    listFragment.appendChild(thumbnail);
   });
+
+  list.appendChild(listFragment);
+
 };
 
 export {addThumbnails};
