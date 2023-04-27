5420e97fbd1802d6d1c50cddb5600ce88fb4a46e
Egor Ermakov <im.egorermakov@gmail.com>
2023-04-27 15:15:45 +0300

    Добавляет предпросмотр загружаемого фото

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { getDataError } from './util.js';
 import { renderForm } from './form.js';
 import { getData } from './api.js';
 import { renderFilters } from './filters.js';
+import './preview.js';
 
 getData(renderFilters, getDataError);
 renderForm();
